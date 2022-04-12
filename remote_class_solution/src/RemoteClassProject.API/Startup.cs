81765fdce000d8cbc6ffb2562e573ccb3579a0c8
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2022-04-12 02:17:31 -0400

    Testando push.

--- remote_class_solution/src/RemoteClassProject.API/Startup.cs
+++ remote_class_solution/src/RemoteClassProject.API/Startup.cs
@@ -23,7 +23,7 @@ namespace RemoteClassProject.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. teste
         public void ConfigureServices(IServiceCollection services)
         {
 
